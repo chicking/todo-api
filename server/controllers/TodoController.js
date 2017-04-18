49c302eea6bf163cef424ddb81106fbefffb479f
gongzza <gz.us.to@gmail.com>
2017-04-19 00:45:46 +0900

    Update todo controller
    added put method

--- server/controllers/TodoController.js
+++ server/controllers/TodoController.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import Todo from '../models/Todo'
-import {wrap} from '../utils'
+import {wrap, error} from '../utils'
 
 var debug = require('debug')('todo-api:TodoController')
 var router = express.Router()
@@ -25,6 +25,33 @@ router.post('/', wrap(async (req, res) => {
   const todo = await Todo.create(reqJson)
 
   res.status(201).json(todo)
+}))
+
+router.put('/:id', wrap(async (req, res) => {
+  debug(`[PUT] /${req.params.id}`)
+
+  const todo = await Todo.findOne({
+    _id: req.params.id,
+    user_id: req.user._id
+  }).exec()
+
+  if (!todo) {
+    throw error(404, 'Not Found')
+  }
+
+  // TODO validate
+
+  if (req.body.content) {
+    todo.content = req.body.content
+  }
+
+  if (typeof req.body.done !== 'undefined' && req.body.done !== null) {
+    todo.done = req.body.done
+  }
+
+  await todo.save()
+
+  res.json(todo)
 }))
 
 module.exports = router
--- test/controllers/TodoController.test.js
+++ test/controllers/TodoController.test.js
@@ -1,6 +1,12 @@
 import test from 'ava'
 import * as utils from '../helpers/utils'
 
+const mockTodo = utils.mock({
+  content: '{{lorem.sentence}}'
+})
+
+let todo_id = null
+
 test.before(() => {
   return utils.removeAll('Todo')
 })
@@ -19,23 +25,45 @@ test('list', async t => {
   const todos = res.body.todos
 
   t.true(Array.isArray(todos))
-  todos.forEach(todo => {
-    t.true(mockTodos.some(mock => mock.content === todo.content))
+  mockTodos.forEach(mock => {
+    t.true(todos.some(todo => todo.content === mock.content))
   })
 })
 
-test.skip('insert', async t => {
-  const mockTodo = utils.mock({
+test.serial('insert', async t => {
+  const res = await utils.auth('post', '/todo', 201)
+    .send(mockTodo)
+
+  const todo = res.body
+  t.truthy(todo._id)
+  t.false(todo.done)
+  t.is(todo.content, mockTodo.content)
+
+  todo_id = todo._id
+})
+
+test.serial('update #content', async t => {
+  const mock = utils.mock({
     content: '{{lorem.sentence}}'
   })
 
-  await utils.auth('post', '/todo', 201)
-    .send(mockTodo)
+  const res = await utils.auth('put', `/todo/${todo_id}`).send(mock)
 
-  const res = await utils.auth('get', '/todo')
-
-  const todos = res.body.todos
-  t.true(todos.some(todo => todo.content === mockTodo.content))
+  const todo = res.body
+  t.is(todo.content, mock.content)
+  t.not(todo.content, mockTodo.content)
 })
-test.todo('update')
-test.todo('delete')
+
+test.serial('update @done', async t => {
+  const mock = {
+    done: !mockTodo.done
+  }
+
+  const res = await utils.auth('put', `/todo/${todo_id}`).send(mock)
+
+  const todo = res.body
+  t.is(todo.done, mock.done)
+  t.not(todo.done, mockTodo.done)
+})
+
+test.skip('delete', async t => {})
