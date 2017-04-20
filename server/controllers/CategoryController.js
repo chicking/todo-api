8e312dc86c4f7cd916198ed941bb2d1c4af3d642
gongzza <gz.us.to@gmail.com>
2017-04-20 23:00:48 +0900

    Update category test
    added CRUD test

--- server/controllers/CategoryController.js
+++ server/controllers/CategoryController.js
@@ -1,14 +1,68 @@
 import express from 'express'
-import {wrap} from '../utils'
 import Category from '../models/Category'
+import {wrap, error} from '../utils'
 
-var debug = require('debug')('todo-api:CategoryController')
+var debug = require('debug')('category-api:CategoryController')
 var router = express.Router()
 
 router.get('/', wrap(async (req, res) => {
-  debug('/')
+  debug('[GET] /')
   const categories = await Category.find({user_id: req.user._id}).exec()
   res.json({categories})
+}))
+
+router.post('/', wrap(async (req, res) => {
+  debug('[POST] /')
+
+  const reqJson = {
+    user_id: req.user._id,
+    title: req.body.title
+  }
+
+  // TODO validate
+
+  const category = await Category.create(reqJson)
+
+  res.status(201).json(category)
+}))
+
+router.put('/:id', wrap(async (req, res) => {
+  debug(`[PUT] /${req.params.id}`)
+
+  const category = await Category.findOne({
+    _id: req.params.id,
+    user_id: req.user._id
+  }).exec()
+
+  if (!category) {
+    throw error(404, 'Not Found')
+  }
+
+  // TODO validate
+
+  if (req.body.title) {
+    category.title = req.body.title
+  }
+
+  await category.save()
+
+  res.json(category)
+}))
+
+router.delete('/:id', wrap(async (req, res) => {
+  debug(`[delete] /${req.params.id}`)
+  const category = await Category.findOne({
+    _id: req.params.id,
+    user_id: req.user._id
+  }).exec()
+
+  if (!category) {
+    throw error(404, 'Not Found')
+  }
+
+  await category.remove()
+
+  res.json(category)
 }))
 
 module.exports = router
--- test/controllers/CategoryController.test.js
+++ test/controllers/CategoryController.test.js
@@ -1,30 +1,65 @@
 import test from 'ava'
 import * as utils from '../helpers/utils'
 
+const mockCategory = utils.mock({
+  title: '{{lorem.word}}'
+})
+
+let category_id = null
+
 test.before(() => {
   return utils.removeAll('Category')
 })
 
-test.beforeEach(async t => {
-  const categories = utils.mocks({
+test('list', async t => {
+  const mockCategories = utils.mocks({
     user_id: 1,
     title: '{{lorem.word}}'
   }, 3)
 
-  return utils.fixtures('Category', categories)
-    .then(docs => {
-      t.context.docs = docs
-    })
-})
+  await utils.fixtures('Category', mockCategories)
 
-test('category', async t => {
   const res = await utils.auth('get', '/category')
-    .expect('Content-Type', /application\/json/)
 
   const categories = res.body.categories
 
   t.true(Array.isArray(categories))
-  t.context.docs.ops.forEach(doc => {
-    t.true(categories.some(category => category.title === doc.title))
+  mockCategories.forEach(mock => {
+    t.true(categories.some(category => category.title === mock.title))
   })
+})
+
+test.serial('insert', async t => {
+  const res = await utils.auth('post', '/category', 201)
+    .send(mockCategory)
+
+  const category = res.body
+  t.truthy(category._id)
+  t.is(category.title, mockCategory.title)
+
+  category_id = category._id
+})
+
+test.serial('update @title', async t => {
+  const mock = utils.mock({
+    title: '{{lorem.sentence}}'
+  })
+
+  const res = await utils.auth('put', `/category/${category_id}`).send(mock)
+
+  const category = res.body
+  t.is(category.title, mock.title)
+  t.not(category.title, mockCategory.title)
+})
+
+test.serial('delete', async t => {
+  // delete
+  await utils.auth('delete', `/category/${category_id}`)
+
+  const res = await utils.auth('get', '/category')
+
+  const categories = res.body.categories
+
+  t.true(Array.isArray(categories))
+  t.true(categories.every(category => category.title !== mockCategory.title))
 })
